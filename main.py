41db262a0ad4b93bf359200886ef222cabbd234e
tsukerin <dustisla@yandex.ru>
2023-09-15 14:06:00 +0300

    Update main.py
    Добавлена возможность указывать цену за сорт яблока))

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 print("Введите количество яблок.")
 a = int(input())
-apples = [0]*a
-print(f"У вас {len(apples)} яблок.")
+print(f"У вас {a} яблок.")
+apples = dict()
 print("Укажите, какого сорта яблоко по очереди:")
-for i in range(0,len(apples)):
-    apples[i] = str(input())    
+for i in range(0,int(a)):
+    type_apple = input()    
 
-print(apples)
+print("Теперь укажите цену за каждое яблоко.")
+for i in range(0,int(a)):
+    price = input()
+
+apples[type_apple] = price
+
+for apples in type_apple, price:
+    print(apples)

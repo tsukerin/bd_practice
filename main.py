4b55a977540fc870ed07978eb2fbcb8a2f2b79c9
setusq <dustisla@yandex.ru>
2023-09-15 21:29:14 +0300

    Доработан код

--- main.py
+++ main.py
@@ -1,33 +1,59 @@
 import time
 
-def calculate_total_cost(apples):
-    total_cost = 0
+def calculate_total_price(apples):
+    total_price = 0
     for price in apples.values():
-        total_cost += int(price)
-    return total_cost
+        total_price += int(price)
+    return total_price
+
+def draw_apple_outline():
+    print("   ******\**")
+    print(" **          **")
+    print("**           **")
+    print("**           **")
+    print("**           **")
+    print("**           **")
+    print(" **         **")
+    print("   *********")
 
 print("Введите количество яблок.")
-a = int(input())
-print(f"У вас {a} яблок.")
-
+apple_count= int(input())
+print(f"У вас {apple_count} яблок.")
 apples = dict()
-
 print("Укажите, какого сорта яблоко по очереди:")
-for i in range(0, a):
+for i in range(0, int(a)):
     type_apple = input()
-    print("Теперь укажите цену за каждое яблоко.")
+    apples[type_apple] = None
+
+print("Теперь укажите цену за каждое яблоко.")
+for type_apple in apples:
     price = input()
     apples[type_apple] = price
 
-print("Информация о ваших яблоках:")
+print("Ваши яблоки и их цены:")
 for type_apple, price in apples.items():
-    print(f"Сорт: {type_apple}, Цена: {price}")
+    print(f"{type_apple}: {price}")
 
-total_cost = calculate_total_cost(apples)
-print(f"Общая стоимость яблок: {total_cost}")
+total_price = calculate_total_price(apples)
+print(f"Общая стоимость всех яблок: {total_price}")
 
-print("Ваш комп взломали: ")
-print("Процент загруженных данных")
-for i in range(1, 101):
-    time.sleep(0.25)
-    print(f"{i}%")
+print("Загрузка данных в базу данных...")
+for i in range(1, 11):
+    time.sleep(0.5)
+    print(f"Загружено {i*10}% данных...")
+print("Загрузка данных завершена.")
+
+print("Напишите пароль содержащий цифры сумма которых даст 13:")
+prol = 1
+mam = 0
+#sdfsdf
+#dfsdfs
+while prol != 13:
+    stree = str(input())
+    for i in range(0,len(stree)):
+        mam += int(stree[i])
+    prol = mam
+    if prol == 13:
+        print("Вы вошли в систему.")
+    else:
+        print("Попробуйте еще раз.")

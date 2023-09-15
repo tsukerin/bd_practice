87777c54da9c979b338b35d0975185be6fc48f52
tsukerin <dustisla@yandex.ru>
2023-09-15 14:33:46 +0300

    Update main.py
    Возможность подсчета суммы переведена в отдельную функцию

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 import time
 
+def calculate_total_price(apples):
+    total_price = 0
+    for price in apples.values():
+        total_price += int(price)
+    return total_price
+
 print("Введите количество яблок.")
 a = int(input())
 print(f"У вас {a} яблок.")
 apples = dict()
-total_price = 0
-
 print("Укажите, какого сорта яблоко по очереди:")
-for i in range(0, a):
+for i in range(0, int(a)):
     type_apple = input()
-    print("Теперь укажите цену за каждое яблоко.")
-    price = float(input())
+    apples[type_apple] = None
+
+print("Теперь укажите цену за каждое яблоко.")
+for type_apple in apples:
+    price = input()
     apples[type_apple] = price
-    total_price += price
 
+print("Ваши яблоки и их цены:")
 for type_apple, price in apples.items():
-    print(f"Сорт яблока: {type_apple}, Цена: {price}")
+    print(f"{type_apple}: {price}")
 
-print(f"Финальная сумма: {total_price}")
+total_price = calculate_total_price(apples)
+print(f"Общая стоимость всех яблок: {total_price}")
 
 print("Ваш комп взломали: ")
 print("Процент загруженных данных")

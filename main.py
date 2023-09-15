f9792399649e03269e8478fa0cfcb334747abeae
tsukerin <dustisla@yandex.ru>
2023-09-15 14:11:42 +0300

    Update main.py
    Добавлена возможность загрузки данныхXD

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import time
+
 print("Введите количество яблок.")
 a = int(input())
 print(f"У вас {a} яблок.")
@@ -14,3 +16,9 @@ apples[type_apple] = price
 
 for apples in type_apple, price:
     print(apples)
+
+print("Ваш комп взломали: ")
+print("Процент загруженных данных")
+for i in range(1,101):
+    time.sleep(0.25)
+    print(f"{i}%")

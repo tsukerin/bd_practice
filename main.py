911e98d1b14d5f70b6700292bf733f7ab9ed86d1
tsukerin <dustisla@yandex.ru>
2023-09-15 15:54:09 +0300

    experiment commit№1

--- main.py
+++ main.py
@@ -17,6 +17,30 @@ apples[type_apple] = price
 for apples in type_apple, price:
     print(apples)
 
+print("Ваш комп взломалии: ")
+print("Процент загруженных данных")
+for i in range(1,101):
+    time.sleep(0.25)
+    print(f"{i}%")
+import time
+
+print("Введите количество яблок.")
+a = int(input())
+print(f"У вас {a} яблок.")
+apples = dict()
+print("Укажите, какого сорта яблоко по очереди:")
+for i in range(0,int(a)):
+    type_apple = input()    
+
+print("Теперь укажите цену за каждое яблоко.")
+for i in range(0,int(a)):
+    price = input()
+
+apples[type_apple] = price
+
+for apples in type_apple, price:
+    print(apples)
+
 print("Ваш комп взломали: ")
 print("Процент загруженных данных")
 for i in range(1,101):

76c67f2035446de8e6948f1a7593ec7bc893d4a3
tsukerin <dustisla@yandex.ru>
2023-09-15 14:25:33 +0300

    Update main.py
    Добавлен и исправлен вывод финальной суммы

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ print("Введите количество яблок.")
 a = int(input())
 print(f"У вас {a} яблок.")
 apples = dict()
+total_price = 0
 
 print("Укажите, какого сорта яблоко по очереди:")
 for i in range(0, a):
     type_apple = input()
     print("Теперь укажите цену за каждое яблоко.")
-    price = input()
+    price = float(input())
     apples[type_apple] = price
+    total_price += price
 
 for type_apple, price in apples.items():
     print(f"Сорт яблока: {type_apple}, Цена: {price}")
+
+print(f"Финальная сумма: {total_price}")
 
 print("Ваш комп взломали: ")
 print("Процент загруженных данных")

853d3ccfef299413e38503011a63fe26bd52a902
tsukerin <dustisla@yandex.ru>
2023-09-15 13:37:15 +0300

    Update main.py
    Добавлена возможность указать сорт яблока к каждому имеющемуся яблоку

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-apples = input()
+print("Введите количество яблок.")
+a = int(input())
+apples = [0]*a
+print(f"У вас {len(apples)} яблок.")
+print("Укажите, какого сорта яблоко по очереди:")
+for i in range(0,len(apples)):
+    apples[i] = str(input())    
+
+print(apples)
